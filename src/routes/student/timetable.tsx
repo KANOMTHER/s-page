cd8be3f301a94f22a9fff2109104705d9fa7b82a
qandle <kimchanidapa@gmail.com>
2024-05-28 02:55:03 +0700

    feat: fetch and student timetable

--- src/routes/student/timetable.tsx
+++ src/routes/student/timetable.tsx
@@ -1,117 +1,74 @@
 import Header from '@/components/header';
 import { columns } from '@/components/student/timetable/columns';
 import Timetable from '@/components/timetable';
-
-const data = [
-	{
-		CourseCode: 'CSE-101',
-		Section: 1,
-		Day: 0,
-		StartTime: '08:00',
-		EndTime: '10:00',
-		Classroom: 'A101',
-		ClassType: 'Lecture',
-	},
-	{
-		CourseCode: 'CSE-102',
-		Section: 1,
-		Day: 1,
-		StartTime: '08:00',
-		EndTime: '10:00',
-		Classroom: 'A102',
-		ClassType: 'Lecture',
-	},
-	// {
-	// 	CourseCode: 'CSE-103',
-	// 	Section: 1,
-	// 	Day: 2,
-	// 	StartTime: '08:00',
-	// 	EndTime: '10:00',
-	// 	Classroom: 'A103',
-	// 	ClassType: 'Lecture',
-	// },
-	{
-		CourseCode: 'CSE-104',
-		Section: 1,
-		Day: 3,
-		StartTime: '08:00',
-		EndTime: '10:00',
-		Classroom: 'A104',
-		ClassType: 'Lecture',
-	},
-	{
-		CourseCode: 'CSE-105',
-		Section: 1,
-		Day: 4,
-		StartTime: '08:00',
-		EndTime: '10:00',
-		Classroom: 'A105',
-		ClassType: 'Lecture',
-	},
-	{
-		CourseCode: 'CSE-106',
-		Section: 1,
-		Day: 5,
-		StartTime: '08:00',
-		EndTime: '10:00',
-		Classroom: 'A106',
-		ClassType: 'Lecture',
-	},
-	{
-		CourseCode: 'CSE-107',
-		Section: 1,
-		Day: 6,
-		StartTime: '08:00',
-		EndTime: '10:00',
-		Classroom: 'A107',
-		ClassType: 'Lecture',
-	},
-	{
-		CourseCode: 'CSE-108',
-		Section: 1,
-		Day: 0,
-		StartTime: '10:00',
-		EndTime: '12:00',
-		Classroom: 'A108',
-		ClassType: 'Lecture',
-	},
-	{
-		CourseCode: 'CSE-109',
-		Section: 1,
-		Day: 1,
-		StartTime: '10:00',
-		EndTime: '12:00',
-		Classroom: 'A109',
-		ClassType: 'Lecture',
-	},
-];
-
-const day_data = Array.from({ length: 7 }, (_, index) => {
-	return {
-		Day: index,
-		Class: [] as {
-			CourseCode: string;
-			Section: number;
-			StartTime: string;
-			EndTime: string;
-			Classroom: string;
-			ClassType: string;
-		}[],
-	};
-});
-
-data.forEach((item) => {
-	day_data[item.Day].Class.push({
-		CourseCode: item.CourseCode,
-		Section: item.Section,
-		StartTime: item.StartTime,
-		EndTime: item.EndTime,
-		Classroom: item.Classroom,
-		ClassType: item.ClassType,
-	});
-});
+import { getRegisteredClass } from '@/model/data/student_data';
+import { useAuth } from '@/hooks/auth_provider';
+// import Swal from 'sweetalert2';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 const STimetable = () => {
+	const auth = useAuth();
+
+	useEffect(
+		() => {
+			if (auth?.user?.role !== 'student') {
+				auth?.navigateTo(auth?.user?.role ?? '');
+			}
+			if (!auth?.user) {
+				auth?.getUser();
+			}
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[auth?.user?.role, auth?.user],
+	);
+
+	const {
+		data,
+		isPending,
+		isError,
+		error,
+	} = useQuery({
+		queryKey: ['timetable', auth?.user?.id],
+		queryFn: () => getRegisteredClass(auth?.user?.id ?? 0, undefined, undefined),
+		enabled: !!auth?.user?.id,
+	});
+
+
+	const day_data = Array.from({ length: 7 }, (_, index) => {
+		return {
+			Day: index,
+			Class: [] as {
+				CourseCode: string;
+				Section: number;
+				StartTime: string;
+				EndTime: string;
+				Classroom: string;
+				ClassType: string;
+			}[],
+		};
+	});
+
+	if (isPending) return <div>Loading...</div>;
+	
+	if (isError) return <div>Error: {error.message}</div>;
+
+	console.log(data)
+	
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	data?.map((item: any) => {
+		day_data[item.Day].Class.push({
+			CourseCode: item.CourseCode,
+			Section: item.Section,
+			StartTime: new Date(item.StartTime).toLocaleTimeString('th-TH', { hour: 'numeric', minute: '2-digit', timeZone: 'UTC' }),
+			EndTime: new Date(item.EndTime).toLocaleTimeString('th-EN', { hour: 'numeric', minute: '2-digit', timeZone: 'UTC' }),
+			Classroom: item.Classroom,
+			ClassType: item.ClassType,
+		});
+	});
+
+	console.log(day_data);
+
 	return (
 		<>
 			<Header title="Timetable" />
