d37fb7e8d8014091202e2d9f06ff02659dd5fbe2
qandle <kimchanidapa@gmail.com>
2024-05-30 05:16:02 +0700

    feat: add getTAClass

--- src/model/data/student_data.ts
+++ src/model/data/student_data.ts
@@ -5,12 +5,22 @@ export const getStudent = async (id: number) => {
   return res.data.message;
 };
 
-// todo: change this to get registered classes
 export const getRegisteredClass = async (id: number, semester?: number, year?: number) => {
-  const res = await axfetch.post('/api/student/schedule', {
+  const res = await axfetch.post('/api/timetable/student', {
     studentId: id,
     semester: semester,
     year: year,
   });
   return res.data.message;
 };
+
+export const getTAClass = async (id: number, semester?: number, year?: number) => {
+  const res = await axfetch.post('/api/timetable/ta', {
+    studentId: id,
+    semester: semester,
+    year: year,
+  });
+  return res.data.message;
+}
+
+// export const getGrade = async ()
